10dfaa50c7cf2a897543ba17311a89119b19ae2b
Hongbin Li <hblee12294@gmail.com>
2020-06-28 00:00:15 -0700

    feat: add some functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+fn get_version() -> u16 {
+    1000
+}
+
+fn usage() {
+    let the_version = get_version();
+
+    println!("tinymd, a markdown compiler written by <YOUR NAME>");
+    println!("Version {}", the_version);
+}
+
 fn main() {
-    println!("Hello, world!");
+    usage();
 }

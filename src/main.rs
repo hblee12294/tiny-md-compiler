4ca3ac8393dbf8d8b39933b868a3c5d8602214f5
Hongbin Li <hblee12294@gmail.com>
2020-06-28 21:25:20 -0700

    feat: try mut variables

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-fn get_version() -> u16 {
-    1000
-}
-
 fn usage() {
-    let the_version = get_version();
+    let mut the_version = env!("CARGO_PKG_VERSION");
+    the_version = "0.2";
 
-    println!("tinymd, a markdown compiler written by <YOUR NAME>");
+    println!("tinymd, a markdown compiler written by Hblee");
     println!("Version {}", the_version);
 }
 
